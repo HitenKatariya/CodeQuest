a83d23b8d6f4c46eaa0f87cebb16117964710466
HitenKatariya <hitenvk22@gmail.com>
2025-06-09 21:21:42 +0530

    feat: Update API base URL to use local development server

--- client/src/api/index.js
+++ client/src/api/index.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || "https://code-quest-flame.vercel.app",
+    baseURL: "http://localhost:5000",
     headers: {
         'Content-Type': 'application/json',
         'Accept': 'application/json'
